18d77f7cf73e5a01bb76eb63af8a875137f3c780
Wilmar Cortes <wfcortes@hotmail.com>
2021-10-14 11:43:21 -0500

    Eliminar Espacio Parqueadero

--- ParkinLot.App/ParkinLot.App.Frontend/Pages/GestorEspacioParqueadero/EliminarEspacioParqueadero.cshtml.cs
+++ ParkinLot.App/ParkinLot.App.Frontend/Pages/GestorEspacioParqueadero/EliminarEspacioParqueadero.cshtml.cs
@@ -4,13 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ParkinLot.App.Dominio;
+using ParkinLot.App.Persistencia;
 
 namespace ParkinLot.App.Frontend.Pages
 {
     public class EliminarEspacioParqueaderoModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioEspacioParqueadero repositorioEspacioParqueadero;
+        public EspacioParqueadero espacioParqueadero { get; set; }
+        public EliminarEspacioParqueaderoModel (IRepositorioEspacioParqueadero repositorioEspacioParqueadero)
         {
+            this.repositorioEspacioParqueadero = repositorioEspacioParqueadero;
+        }
+        public void OnGet(int Id)
+        {
+            espacioParqueadero = repositorioEspacioParqueadero.GetEspacioParqueadero(Id);
+        }
+        public IActionResult OnPost(EspacioParqueadero espacioParqueadero)
+        {
+            try {
+                repositorioEspacioParqueadero.DeleteEspacioParqueadero(espacioParqueadero.Id);
+                return RedirectToPage("./ListaEspaciosParqueadero");
+            }
+            catch {
+                return RedirectToPage("../Error");
+            }
         }
     }
 }
--- ParkinLot.App/ParkinLot.App.Frontend/Pages/GestorVehiculo/AgregarVehiculo.cshtml.cs
+++ ParkinLot.App/ParkinLot.App.Frontend/Pages/GestorVehiculo/AgregarVehiculo.cshtml.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ParkinLot.App.Dominio;
+using ParkinLot.App.Persistencia;
 
 namespace ParkinLot.App.Frontend.Pages
 {
+    
     public class AgregarVehiculoModel : PageModel
     {
+        private readonly IRepositorioVehiculo repositorioVehiculo;
+        private readonly IRepositorioDuenoVehiculo repositorioDuenoVehiculo;
+        
+        public SelectList duenoVehiculo { get; set; }
+        public AgregarVehiculoModel(IRepositorioVehiculo repositorioVehiculo, IRepositorioDuenoVehiculo repositorioDuenoVehiculo)
+        {
+            this.repositorioVehiculo = repositorioVehiculo;
+            this.repositorioDuenoVehiculo = repositorioDuenoVehiculo;
+            
+            //duenoVehiculo =  new SelectList(repositorioDuenoVehiculo.GetAllDuenosVehiculos(),nameof(duenoVehiculo.Id),nameof(duenoVehiculo.Nombres));
+
+        }
         public void OnGet()
         {
         }
     }
 }
+
